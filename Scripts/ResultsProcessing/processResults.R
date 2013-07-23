d07edbe08b03613c24e0486aa654d09d7ea1b436
Jacopo De Stefani <jacopo.de.stefani@ulb.ac.be>
2013-07-24 01:09:01 +0200

    Updated results analysis script.

--- Scripts/ResultsProcessing/processResults.R
+++ Scripts/ResultsProcessing/processResults.R
@@ -1,19 +1,4 @@
-relativeOccupationPercentile <- function(relativeOccupationMatrix,percentile){
-  percentileOccupation <- relativeOccupationMatrix[relativeOccupationMatrix[,1]>=percentile &
-                                                   relativeOccupationMatrix[,2]>=percentile &
-                                                   relativeOccupationMatrix[,3]>=percentile &
-                                                   relativeOccupationMatrix[,4]>=percentile,1]
-  
-  # If a certain percentile is never reached, 
-  # return the time corresponding to the simulation duration + 1
-  # to signal infeasibily, without occuring in NA.
-  if(length(percentileOccupation) == 0){
-    return(length(relativeOccupationMatrix[,1])+1)
-  }
-  else{
-    return(percentileOccupation)
-  }
-}
+library(ggplot2)
 
 boxplotToPdf <- function(file,data,stringTitle,stringXLabel,color,pdfHeight,pdfWidth){
   pdf(file)
@@ -81,7 +66,7 @@ plotRobotOnTarget <- function(onSpotPlot){
   #Plot max error 
   pdf("RobotOnTarget.pdf")
   xRange <- range(0,dim(onSpotPlot)[1])
-  yRange <- range(0,5)
+  yRange <- range(0,7)
   nPlots <- dim(onSpotPlot)[2]
   plotColors <- rainbow(nPlots) 
   lineType <- c(1,2,4,2,1,5) 
@@ -136,7 +121,9 @@ meanInChain <- apply(inChain,1,mean)
 meanOnTarget <-apply(onTarget,1,mean)
 #Bind toghether the elements
 inChainPlot <- cbind(inChainPlot,meanInChain)
+colnames(inChainPlot) <- c("0%","25%", "50%","75%","100%","Mean")
 onTargetPlot <- cbind(onTargetPlot,meanOnTarget)
+colnames(onTargetPlot) <- c("0%","25%", "50%","75%","100%","Mean")
 
 print("[STATUS] - Create reporting data frame")
 #Create data frame for reporting
@@ -153,10 +140,32 @@ report["Max","Robots in Chain"] <- max(metrics["Robots in Chain",])
 report["Max","Completion Time"] <- max(metrics["Completion Time",])
 #Write reporting dataframe
 write.table(report,"Results.stat",sep="\t",row.names=TRUE,col.names=TRUE)
+#write(paste("Correlation (Pearson)",cor(x=metrics["Robots in Chain",],y=metrics["Completion Time",], use="all.obs", method="pearson"),sep="\t"),"Result.stat",append = TRUE)
+#write(paste("Correlation (Spearman)",cor(x=metrics["Robots in Chain",],y=metrics["Completion Time",], use="all.obs", method="spearman"),sep="\t"),"Result.stat",append = TRUE)
+#write(paste("Correlation (Kendall)",cor(x=metrics["Robots in Chain",],y=metrics["Completion Time",], use="all.obs", method="kendall"),sep="\t"),"Result.stat",append = TRUE)
 
 #Plot box plots and empirical cumulative density functions
-boxplotToPdf("BoxplotTime.pdf",metrics["Completion Time",],paste("Boxplot of completion times across",length(resultsFiles),"trials"),"Simulation steps","blue")
-boxplotToPdf("BoxplotRobots.pdf",metrics["Robots in Chain",],paste("Boxplot of number of robots in chain across",length(resultsFiles),"trials"),"Number of robots","orange")
+#boxplotToPdf("BoxplotTime.pdf",metrics["Completion Time",],paste("Boxplot of completion times across",length(resultsFiles),"trials"),"Simulation steps","blue")
+#boxplotToPdf("BoxplotRobots.pdf",metrics["Robots in Chain",],paste("Boxplot of number of robots in chain across",length(resultsFiles),"trials"),"Number of robots","orange")
+
+# Add boxplots to a scatterplot
+# Scatterplot
+pdf("ResultsDistribution.pdf")
+par(fig=c(0,0.8,0,0.8), new=TRUE)
+plot(x=metrics["Robots in Chain",],y=metrics["Completion Time",],col=rgb(0,100,0,80,maxColorValue=255), pch=19 , xlab="Robots in Chain",
+     ylab="Completion Time")
+# Fit lines
+abline(lm(metrics["Completion Time",]~metrics["Robots in Chain",]), col="red") # regression line (y~x) 
+lines(lowess(metrics["Robots in Chain",],metrics["Completion Time",]), col="blue") # lowess line (x,y)
+# Boxplot X-Axis
+par(fig=c(0,0.8,0.55,1), new=TRUE)
+boxplot(metrics["Robots in Chain",], horizontal=TRUE, axes=FALSE)
+# Boxplot Y-Axis
+par(fig=c(0.65,1,0,0.8), new=TRUE)
+boxplot(metrics["Completion Time",], axes=FALSE)
+# Title
+mtext(expression(bold("Results Distribution")), side=3, outer=TRUE, line=-3,cex=1.5)
+dev.off()
 
 #Plot empirical CDF of completion times
 pdf("EcdfTime.pdf")
