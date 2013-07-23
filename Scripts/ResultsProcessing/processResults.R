02404656f745e11ad343667dfa97c7c483bad18f
Jacopo De Stefani <jacopo.de.stefani@ulb.ac.be>
2013-07-23 03:51:02 +0200

    Started report revision. Implemented working Result Analysis script.

--- Scripts/ResultsProcessing/processResults.R
+++ Scripts/ResultsProcessing/processResults.R
@@ -15,14 +15,15 @@ relativeOccupationPercentile <- function(relativeOccupationMatrix,percentile){
   }
 }
 
-boxplotToPdf <- function(file,data,stringTitle,stringXLabel,pdfHeight,pdfWidth){
+boxplotToPdf <- function(file,data,stringTitle,stringXLabel,color,pdfHeight,pdfWidth){
   pdf(file)
-  boxplot(as.data.frame(data),horizontal=TRUE,main=stringTitle,col=terrain.colors(length(colNames)),las=1)
+  boxplot(as.data.frame(data),horizontal=TRUE,main=stringTitle,col=color,las=1)
   title(xlab=stringXLabel)
   dev.off()
 }
 
 extractMetrics <- function(inputFile){
+  print(inputFile)
   #Read data
   inputData <- read.table(inputFile, header = TRUE)
   
@@ -32,34 +33,75 @@ extractMetrics <- function(inputFile){
   #Extract the completion time
   completionTime <- inputData[dim(inputData)[1],1]
   
-  returnValues <- list(robotsInChain,completionTime)
+  returnValues <- c(as.numeric(robotsInChain),as.numeric(completionTime))
   return(returnValues)
 }
 
-createTexGraph <- function(xlabel,ylabel,height,width,xdataset,ydataset){
-  #Output values to console -> Redirect to file to obtain a valid tex file
-  sink("./results/Occupation.tex",append=TRUE)
-  sprintf("\\begin{tikzpicture}\n")
-  sprintf("  \\begin{axis}[\n")
-  sprintf("	xlabel=%s,\n",xlabel)
-  sprintf("	ylabel=%s$,\n",ylabel)
-  sprintf("	height=%fcm,\n",height)
-  sprintf("	width=%fcm,\n",width)
-  sprintf("	grid=major,\n")
-  sprintf("	legend style={at={(0,0)},\n")
-  sprintf("	anchor=north,legend columns=-1},")
-  sprintf("	]\n") 
-  sprintf("\\addplot [color=green,mark=none,very thick] coordinates {\n")
-  sprintf("(%d,%f)\n",xdataset,ydataset)
-  sprintf("    };\n")
-  #sprintf("\\addlegendentry{"+str(legend[index])+"}\n")
-  #sprintf("\\legend{")
-  #for element in legend:
-  #  sprintf(str(element)+",")
-  #sprintf("}\n") 
-  sprintf("	\\end{axis}\n")
-  sprintf("\\end{tikzpicture}\n")
+padVector <- function(vector,desiredLength){
+  if(length(vector) < desiredLength){
+    paddingLength = desiredLength - length(vector)
+    return(append(vector,rep(vector[length(vector)],paddingLength)))
+  }
+  return(vector)
 }
+
+extractColumn <- function(inputFile,index,maxDuration){
+  #Read data
+  inputData <- read.table(inputFile, header = TRUE)
+  return(padVector(inputData[,index],maxDuration))
+}
+
+
+plotRobotInChain <- function(inChainPlot){
+  #Plot max error 
+  pdf("RobotInChain.pdf")
+  xRange <- range(0,dim(inChainPlot)[1])
+  yRange <- range(0,50)
+  nPlots <- dim(inChainPlot)[2]
+  plotColors <- rainbow(nPlots) 
+  lineType <- c(1,2,4,2,1,5) 
+  plotChar <- seq(18,18+nPlots,1)
+  
+  plot(xRange,yRange, type="n", xlab="Simulation steps", ylab="Robots in chain",main=paste("Time evolution of number of robots in chain across",length(resultsFiles),"trials"))   
+  
+  #Add lines
+  matlines(inChainPlot, type="l" , lwd=1.5, lty=lineType, col=plotColors, pch=plotChar)
+  
+  #Add a title
+  #title(graphTitle)
+  
+  #Add a legend 
+  legend(x="bottomleft", colnames(inChainPlot), cex=0.8, col=plotColors, lty=lineType, title="Percentiles")
+  dev.off()
+  
+  write.table(inChainPlot,"RobotInChainPlots.plt",sep="\t",row.names=FALSE,col.names=TRUE)
+}
+
+plotRobotOnTarget <- function(onSpotPlot){
+  #Plot max error 
+  pdf("RobotOnTarget.pdf")
+  xRange <- range(0,dim(onSpotPlot)[1])
+  yRange <- range(0,5)
+  nPlots <- dim(onSpotPlot)[2]
+  plotColors <- rainbow(nPlots) 
+  lineType <- c(1,2,4,2,1,5) 
+  plotChar <- seq(18,18+nPlots,1)
+  
+  plot(xRange,yRange, type="n", xlab="Simulation steps", ylab="Robots on spot",main=paste("Time evolution of number of robots on target across",length(resultsFiles),"trials"))   
+
+  #Add lines
+  matlines(onSpotPlot, type="l" , lwd=1.5, lty=lineType, col=plotColors, pch=plotChar)
+
+  #Add a title
+  #title(graphTitle)
+
+  #Add a legend 
+  legend(x="bottomleft", colnames(onSpotPlot), cex=0.8, col=plotColors, lty=lineType, title="Percentiles")
+  dev.off()
+
+  write.table(onSpotPlot,"RobotOnSpotPlots.plt",sep="\t",row.names=FALSE,col.names=TRUE)
+}
+
 
 #computeQuantileByRow <- function(matrix){
 #  return t(apply(matrix,1,quantile))
@@ -68,41 +110,70 @@ createTexGraph <- function(xlabel,ylabel,height,width,xdataset,ydataset){
 
 
 #Set the directory containing the experiment results as current working directory
-setwd("/home/deste/workspace/INFO-H-414/Results/")
+setwd("Results/ResultsDS")
 #List all the files containing information about occupation
 resultsFiles <- list.files(pattern = "\\.txt$")
 #Process every single file separately
-metrics <- lapply(resultsFiles,extractMetrics)
+print("[STATUS] - Processing results file")
+metrics <- sapply(resultsFiles,extractMetrics)
 #Transform processing results into dataframe
-metrics <- data.frame(matrix(unlist(metrics), nrow=length(metrics)))
-colnames(metrics) <- c("Robots in Chain","Completion Time")
+metrics <- as.matrix(metrics)
+rownames(metrics) <- c("Robots in Chain","Completion Time")
+duration <- max(metrics["Completion Time",])
+
+inChain <- lapply(resultsFiles,extractColumn,index=2,maxDuration=duration)
+onTarget <- lapply(resultsFiles,extractColumn,index=3,maxDuration=duration)
+inChain <- data.frame(matrix(unlist(inChain), ncol=length(inChain)))
+onTarget <- data.frame(matrix(unlist(onTarget), ncol=length(onTarget)))
+
+#print("[STATUS] - Computing quantiles & mean")
+#Given the matrix containing the integrated errors for each trial as column, compute the quantiles per row.
+#That corresponds to obtain the quantiles at each time step
+inChainPlot <- t(apply(inChain,1,quantile))
+onTargetPlot <- t(apply(onTarget,1,quantile))
+#Compute row by row mean of the integrated error of every single trial
+meanInChain <- apply(inChain,1,mean)
+meanOnTarget <-apply(onTarget,1,mean)
+#Bind toghether the elements
+inChainPlot <- cbind(inChainPlot,meanInChain)
+onTargetPlot <- cbind(onTargetPlot,meanOnTarget)
+
+print("[STATUS] - Create reporting data frame")
 #Create data frame for reporting
 report <- data.frame()
-report["Robots in Chain","Mean"] <- mean(metrics[,"Robots in Chain"])
-report["Completion Time","Mean"] <- mean(metrics[,"Completion Time"])
-report["Robots in Chain","Standard Deviation"] <- sd(metrics[,"Robots in Chain"])
-report["Completion Time","Standard Deviation"] <- sd(metrics[,"Completion Time"])
-report["Robots in Chain","Median"] <- median(metrics[,"Robots in Chain"])
-report["Completion Time","Median"] <- median(metrics[,"Completion Time"])
-report["Robots in Chain","Min"] <- min(metrics[,"Robots in Chain"])
-report["Completion Time","Min"] <- min(metrics[,"Completion Time"])
-report["Robots in Chain","Max"] <- max(metrics[,"Robots in Chain"])
-report["Completion Time","Max"] <- max(metrics[,"Completion Time"])
-
+report["Mean","Robots in Chain"] <- mean(metrics["Robots in Chain",])
+report["Mean","Completion Time"] <- mean(metrics["Completion Time",])
+report["Standard Deviation","Robots in Chain"] <- sd(metrics["Robots in Chain",])
+report["Standard Deviation","Completion Time"] <- sd(metrics["Completion Time",])
+report["Median","Robots in Chain"] <- median(metrics["Robots in Chain",])
+report["Median","Completion Time"] <- median(metrics["Completion Time",])
+report["Min","Robots in Chain"] <- min(metrics["Robots in Chain",])
+report["Min","Completion Time"] <- min(metrics["Completion Time",])
+report["Max","Robots in Chain"] <- max(metrics["Robots in Chain",])
+report["Max","Completion Time"] <- max(metrics["Completion Time",])
 #Write reporting dataframe
 write.table(report,"Results.stat",sep="\t",row.names=TRUE,col.names=TRUE)
 
 #Plot box plots and empirical cumulative density functions
-boxplotToPdf("BoxplotTime.pdf",metrics[,"Completion Time"],paste("Boxplot of completion times across",length(resultsFiles),"trials"),"Simulation steps")
-boxplotToPdf("BoxplotRobots.pdf",metrics[,"Robots in chain"],paste("Boxplot of number of robots in chain across",length(resultsFiles),"trials"),"Number of robots")
+boxplotToPdf("BoxplotTime.pdf",metrics["Completion Time",],paste("Boxplot of completion times across",length(resultsFiles),"trials"),"Simulation steps","blue")
+boxplotToPdf("BoxplotRobots.pdf",metrics["Robots in Chain",],paste("Boxplot of number of robots in chain across",length(resultsFiles),"trials"),"Number of robots","orange")
 
 #Plot empirical CDF of completion times
 pdf("EcdfTime.pdf")
 graphTitle <- paste("Empirical cdf of completion time across",length(resultsFiles),"trials")
-plot(ecdf(metrics[,"Completion Time"]),col="orange",xlab="Simulation steps")
+plot(ecdf(metrics["Completion Time",]),col="blue",xlab="Simulation steps",main=paste("Empirical cdf of completion times across",length(resultsFiles),"trials"))
 title(str(graphTitle))
 dev.off()
 
+#Plot empirical CDF of completion times
+pdf("EcdfRobots.pdf")
+graphTitle <- paste("Empirical cdf of completion time across",length(resultsFiles),"trials")
+plot(ecdf(metrics["Robots in Chain",]),col="blue",xlab="Simulation steps",main=paste("Empirical cdf of completion times across",length(resultsFiles),"trials"))
+title(str(graphTitle))
+dev.off()
+
+plotRobotInChain(inChainPlot)
+plotRobotOnTarget(onTargetPlot)
 
 #Output analysis result
 #sprintf("Time required to get 25%% coverage: %.0f",firstQuartileOccupation[1])
